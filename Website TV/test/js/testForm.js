e7696961fdf3c591a7bb6e3602ca3c44cb122836
kendrick2k10 <123327735+kendrick2k10@users.noreply.github.com>
2024-08-28 12:40:51 +0800

    Add files via upload

---
+++ Website TV/test/js/testForm.js
@@ -0,0 +1,21 @@
+let category = []
+
+const genre = []
+
+const trendItems = []
+
+
+const render = {}
+
+
+//Where Everything Starts
+
+document.body.innerHTML += bodyHTML
+
+sortLatestRender()
+topRatedRender()
+
+headerFunctions()
+
+eventListener()
+
